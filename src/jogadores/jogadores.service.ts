d24201f74f52d1a623c489855f98cead05cc37c8
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-11-30 20:35:11 -0300

    Ajustando controller de jogadores

---
+++ src/jogadores/jogadores.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class JogadoresService {
+  private readonly logger = new Logger(JogadoresService.name);
+  private jogadores: Jogador[] = [];
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    this.logger.log(`Cria jogador dto: ${criarJogadorDto}`);
+    await this.criar(criarJogadorDto);
+  }
+
+  private criar(criarJogadorDto: CriarJogadorDto): void {
+    const { nome, email, telefoneCelular } = criarJogadorDto;
+    const jogador: Jogador = {
+      _id: uuidv4(),
+      nome,
+      telefoneCelular,
+      email,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFotoJogador: 'www.google.com.br/foto123.jpg',
+    };
+    this.jogadores.push(jogador);
+  }
+}
