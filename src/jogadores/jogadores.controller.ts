da55f917fed525958dfff67234c8f9257c6d32bd
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-11-30 20:17:11 -0300

    Criando controller de jogadores

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+  @Post()
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    const { email } = criarJogadorDto;
+    return JSON.stringify(`{
+      nome: ${email},
+    }`);
+  }
+}
