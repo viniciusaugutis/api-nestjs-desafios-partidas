cef584c541df2a03a1a97357207cdf438a445ed0
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-11-30 21:19:03 -0300

    Estruturando mongoose e schema de jogador

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('string', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
